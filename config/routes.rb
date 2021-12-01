a6f64c8710b3ae5b013693d58965d32cba58770c
Stephen <swrobbin@gmail.com>
2021-12-01 10:20:06 -0500

    assignment completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :spices
+  # , only: [:index, :show, :create, :update, :delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
